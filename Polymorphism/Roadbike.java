445fd8797dab7a5f014cfc76052e18f370b1b61b
sanketjain <mr.sanketjain@gmail.com>
2016-12-04 21:20:47 -0500

    Polymorphism
    Polymorphism example from Oracle website

---
+++ Polymorphism/Roadbike.java
@@ -0,0 +1,20 @@
+
+public class RoadBike extends Bicycle{
+
+    private int tireWidth;
+
+    public RoadBike(int startCadence, int startSpeed, int startGear, int newTireWidth){
+        super(startCadence, startSpeed, startGear);
+        this.setTireWidth(newTireWidth);
+    }
+
+    public int getTireWidth(){ return this.tireWidth; }
+    public void setTireWidth(int newTireWidth){ this.tireWidth = newTireWidth; }
+    public void printDescription(){
+        super.printDescription();
+        System.out.println("The RoadBike" + " has " + getTireWidth() + " MM tires.");
+    }
+}
+
+
+
