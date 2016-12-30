b1f9577be00ec591a32e6f30b9062fa6269e37e6
sanketjain <mr.sanketjain@gmail.com>
2016-12-30 11:56:01 -0500

    redTicket
    redTicket

---
+++ Logic-1/redTicket.java
@@ -0,0 +1,11 @@
+public int redTicket(int a, int b, int c) {
+  if (a==b && b==c && c==2){
+    return 10;
+  } else if (a==b && b==c){
+    return 5;
+  } else if(a!=b && c!=a){
+    return 1;
+  } else { 
+    return 0;
+  }
+}
