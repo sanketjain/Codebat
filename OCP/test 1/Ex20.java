f4f8e8d65c24091b982fa5dce047fc137db7f8cc
sanketjain <mr.sanketjain@gmail.com>
2017-01-09 10:51:30 -0500

    Exception
    Which block throws exception

---
+++ OCP/test 1/Ex20.java
@@ -0,0 +1,18 @@
+class Ex20 {
+	public static void main(String[] args){
+		System.out.print("a");
+		try {
+			System.out.print("b");
+			throw new IllegalArgumentException();
+		} catch (IllegalArgumentException e) {
+			System.out.print("c");
+			throw new RuntimeException("1");
+		} catch(RuntimeException e){
+			System.out.print("d");
+			throw new RuntimeException("2");
+		} finally {
+			System.out.print("e");
+			throw new RuntimeException("3");
+		}
+	}
+}
--- OCP/test 1/Supper.java
+++ OCP/test 1/Supper.java
@@ -2,7 +2,7 @@ class Supper {
 	public static void eat() throws IOException{
 		try{
 			System.out.println("1");
-			throw new IOException
+			throw new IOException();
 		} catch (IOException e) {
 			System.out.println("2");
 			throw e;
