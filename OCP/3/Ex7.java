4dafc6dbc7da2c910f436c255807b3c275b9e642
sanketjain <mr.sanketjain@gmail.com>
2017-01-10 10:23:09 -0500

    String
    Length, charAt and Exception in a String

---
+++ OCP/3/Ex7.java
@@ -0,0 +1,8 @@
+public class Ex7 {
+	public static void main(String[] args){
+		String letters = "abcdef";
+		System.out.println(letters.length());
+		System.out.println(letters.charAt(3));
+		System.out.println(letters.charAt(6));
+	}
+}
