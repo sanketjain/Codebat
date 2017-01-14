47127b0ebbbda2333794321dff2592756e784c67
sanketjain <mr.sanketjain@gmail.com>
2017-01-13 19:10:45 -0500

    Never stop
    Instance initialiser class the constructor. Constructor calls the
    instance initialiser.

---
+++ OCP/test 2/Lion.java
@@ -0,0 +1,12 @@
+public class Lion{
+	Lion l = new Lion();
+	public static void main(String[] args){
+		new Lion();
+	}
+	public void roar(){
+		Lion l = new Lion();
+		if(l==l){
+			System.out.println("roar!");
+		}
+	}
+}
