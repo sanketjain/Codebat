02750882abb844437386ec6caf4fd041c4c6462a
sanketjain <mr.sanketjain@gmail.com>
2017-01-14 16:13:17 -0500

    Ternary
    How to use Ternary Expression

---
+++ OCP/test 4 other/TernaryEx.java
@@ -0,0 +1,12 @@
+public class TernaryEx{
+	static int x = 10;
+	public static void main(String[] args){
+		if(x>10){
+			System.out.println(">");
+		} else if(x<10){
+			System.out.println("<");
+		} else {
+			System.out.println("=");
+		}
+	}
+}
---
+++ OCP/test 4 other/TernaryEx2.java
@@ -0,0 +1,6 @@
+public class TernaryEx2{
+	public static void main(String[] args){
+		int x = 10;
+		System.out.println(x>10 ? ">" : x<10 ? "<" : "=");
+	}
+}
