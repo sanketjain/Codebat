f3088ce2d2a994ffa8e1a9677783c1cc48f943ef
sanketjain <mr.sanketjain@gmail.com>
2017-01-14 18:49:34 -0500

    Float
    Float conversion

---
+++ OCP/1/Ex.java
@@ -0,0 +1,11 @@
+public class Ex{
+	public static void main(String[] args){
+
+		float value1 = 102;
+		float value2 = (int)102.0;
+		float value3 = 1f * 0.0;
+		float value4 = 1f * (short)0.0;
+		float value5 = 1f * (boolean)0;
+		System.out.println();
+	}
+}
