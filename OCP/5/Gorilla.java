b307a11adfeae2ff74483eb2df16f04a218930de
sanketjain <mr.sanketjain@gmail.com>
2017-01-18 10:48:48 -0500

    Super Keyword
    Usage of Super Keyword. If you don’t have super key, then you may or
    may not be allowed to compile

--- OCP/5/Gorilla.java
+++ OCP/5/Gorilla.java
@@ -4,14 +4,14 @@ public class Gorilla extends Animal3 {
 		super (age, "Gorilla");
 	}
 
-	public Gorilla() {
-		super(5);
-	}
+ 	public Gorilla() {
+ 		super(5);
+ 	}
 
 	public static void main(String[] args){
 		Gorilla gorilla1 = new Gorilla(10);
 		System.out.println(gorilla1.age);
-		Gorilla gorilla2 = new Gorilla();
-		System.out.println(gorilla2.age);
+//		Gorilla gorilla2 = new Gorilla();
+//		System.out.println(gorilla2.age);
 	}
 }
