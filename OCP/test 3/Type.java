a72d6475129c6fe0f258e3209c5f2b65679b65ed
sanketjain <mr.sanketjain@gmail.com>
2017-01-22 17:33:54 -0500

    Local vs Instance Variable
    Difference between class, local and instance variable.

---
+++ OCP/test 3/Type.java
@@ -0,0 +1,10 @@
+public class Type{
+	int integer; //Instance Variable
+	static int num; //Class Variable
+	public Type(){
+		int type; //Local Variable
+	}
+	
+	public static void main(String[] args){
+	}
+}
