5e1f31e79e2022f1205933d254a2c61c0b7a929b
sanketjain <mr.sanketjain@gmail.com>
2017-01-30 11:57:27 -0500

    Has 2 or 3
    Given an int array length 2, return true if it contains a 2 or a 3

---
+++ Array-1/has23.java
@@ -0,0 +1,9 @@
+public boolean has23(int[] nums) {
+  if((nums[0] == 2) || (nums[1] == 2)){
+    return true;
+  } else if((nums[0] == 3) || (nums[1] == 3)){
+    return true;
+  } else {
+    return false;
+  }
+}
