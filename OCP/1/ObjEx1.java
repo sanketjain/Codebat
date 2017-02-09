002258896aff74712e809244bbdb809bd8659663
sanketjain <mr.sanketjain@gmail.com>
2017-02-09 11:30:47 -0500

    Autoboxing and Unboxing
    Example of auto-boxing and unboxing

---
+++ OCP/1/ObjEx1.java
@@ -0,0 +1,11 @@
+public class ObjEx1{
+
+		int y = 0;
+		System.out.println(y);
+
+	public static void main(String[] args){
+		ObjEx1 obj = new ObjEx1();
+		int x = new Integer(10); //Wrapper class Integer, primitive int
+		System.out.println(x);	//unboxing
+	}
+}
