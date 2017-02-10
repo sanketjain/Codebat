578c4342b0ce8c630eb61126272b59f2df9df24a
sanketjain <mr.sanketjain@gmail.com>
2017-02-10 07:59:05 -0500

    print max value
    Call MAX_VALUE method on Integer, Long, Float and Double Wrapper class
    objects

---
+++ OCP/1/findmaxvalue.java
@@ -0,0 +1,8 @@
+public class findmaxvalue{
+	public static void main(String[] args){
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+	}
+}
