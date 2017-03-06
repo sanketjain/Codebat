eb9270071869a5919b150c5e220c5ed15a1d40c9
sanketjain <mr.sanketjain@gmail.com>
2017-03-06 18:04:40 -0500

    Maximum Draws of socks
    Jim is off to a party and is searching for a matching pair of socks.
    His drawer is filled with socks, each pair of a different color. In its
    worst case scenario, how many socks (x) should Jim remove from his
    drawer until he finds a matching pair?

---
+++ Hackerrank/Mathematics/MaximumDraws.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class MaximumDraws {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+       	int t = in.nextInt();
+       	int x;
+
+       	for(int a=0; a<t; a++){ 
+       		x = in.nextInt();
+       		System.out.println(x+1);
+       	}
+
+
+    }
+}
