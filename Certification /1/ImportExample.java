f4e45d63ac8765014ac1387069aab99d2a5ca8b0
sanketjain <mr.sanketjain@gmail.com>
2017-03-13 16:31:32 -0400

    Print random number
    How to print random numbers within a range

--- Certification /1/ImportExample.java
+++ Certification /1/ImportExample.java
@@ -4,7 +4,9 @@ import java.util.Random;
 
 public class ImportExample{
 	public static void main(String[] args){
-		Random r = new Random();
-		System.out.println(r.nextInt(10));
+		for(int i=0; i<100; i++){
+			Random r = new Random();
+			System.out.println(r.nextInt(10));			
+		}
 	}
 }
