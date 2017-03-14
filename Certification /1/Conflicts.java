8d7ef53262a40d28398666b157d874f3f4c5f77c
sanketjain <mr.sanketjain@gmail.com>
2017-03-14 09:16:04 -0400

    Import Conflicts
    Use of wildcards can sometimes lead to conflict

--- Certification /1/Conflicts.java
+++ Certification /1/Conflicts.java
@@ -1,9 +1,10 @@
 
-import java.util.Date;
+import java.util.*;
+import java.sql.*;
 //If we really do want to use Date from two different packages then:
 
 public class Conflicts{
 	Date date;
-	java.sql.Date sqlDate;
+	//java.sql.Date sqlDate;
 }
 
